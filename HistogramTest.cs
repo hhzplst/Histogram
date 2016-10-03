9016712238fde52c21b9a9957718455829c128bd
Ivy Chang <ivycwq@icloud.com>
2016-10-03 14:29:12 -0700

    testing methods

---
+++ HistogramTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Histogram {
+    public class HistogramTest {
+        public static void Main(string[] args) {
+
+
+            Histogram myHistogram = new Histogram(0, 1);
+            myHistogram.PlotFrequency();
+        }        
+    }
+}
