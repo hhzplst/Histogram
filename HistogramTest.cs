e09083b3835cfbd9e845895a56173bc7c2edf580
Ivy Chang <ivycwq@icloud.com>
2016-10-05 11:52:59 -0700

    adding test cases

--- HistogramTest.cs
+++ HistogramTest.cs
@@ -4,9 +4,12 @@ namespace Histogram {
     public class HistogramTest {
         public static void Main(string[] args) {
 
-
             Histogram myHistogram = new Histogram(0, 1);
-            myHistogram.PlotFrequency();
+            Random randomObj = new Random();
+            for (int i = 0; i < 10000; i++)
+                myHistogram.Add(randomObj.NextDouble());
+            myHistogram.PlotFrequency();  
+            myHistogram.PlotCumulative();     
         }        
     }
 }
